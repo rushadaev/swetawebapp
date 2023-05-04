fde472fbe8cb7b731a142a7df70435c8638f7e15
Ruslan Shadaev <shadaev@leber.ru>
2023-05-04 16:13:06 +0300

    feat: autopay, request change by click

--- src/components/RequestCreateButton/RequestCreateBlock.vue
+++ src/components/RequestCreateButton/RequestCreateBlock.vue
@@ -11,7 +11,7 @@
         Загрузка...
       </div>
     </div>
-    <div v-else-if="profile.subscribed || profile.request_count > 0" @click="$router.push('/createresponse')" class="InfoBlockItem InfoBlockItem__material">
+    <div v-else-if="profile.subscribed || profile.request_count < 5" @click="$router.push('/createresponse')" class="InfoBlockItem InfoBlockItem__material">
       <div class="icon icon__material">
         <svg fill="none" height="28" viewBox="0 0 28 28" width="28" xmlns="http://www.w3.org/2000/svg" class="zGiP"><circle cx="14" cy="14" r="11" stroke="currentColor" stroke-width="1.6"></circle><g fill="currentColor"><path d="M13.2 18.5a.8.8 0 0 0 1.6 0zm1.6-9a.8.8 0 0 0-1.6 0zm0 9v-9h-1.6v9z"></path><path d="M18.5 14.8a.8.8 0 0 0 0-1.6zm-9-1.6a.8.8 0 0 0 0 1.6zm9 0h-9v1.6h9z"></path></g></svg>
       </div>
